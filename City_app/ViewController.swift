d829efb1bfcac473b980b3dd75b83e9f220e6a58
GeorgiSwiftDeveloper <malkhasyangeorge@gmail.com>
2018-12-11 20:17:11 -0800

    Initial Commit

---
+++ City_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  City_app
+//
+//  Created by Georgi Malkhasyan on 12/11/18.
+//  Copyright © 2018 Adamyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
